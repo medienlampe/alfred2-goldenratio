a011b32687c9c74a17f98beacb76b92e09946ba8
Sven Bischoff <coding@multigeneris.org>
2015-02-22 20:17:50 +0100

    - removed title line because it's not adding any value (thanks Florian!) - cleanup

--- workflow/gr.sh
+++ workflow/gr.sh
@@ -5,7 +5,6 @@ source feedback.sh
 goldenratio_long=$(bc <<< "$1"/1.618)
 goldenratio_short=$(bc <<< "$1"/2.618)
 
-result_title=$(add_item "" "Golden ratio of $1:" "" "" "no" "$1")
 result_long=$(add_item "" "$goldenratio_long" "Golden Ratio (longer side)" "$goldenratio_long" "yes")
 result_short=$(add_item "" "$goldenratio_short" "Golden Ratio (shorter side)" "$goldenratio_short" "yes")
-output_xml "$result_title" "$result_long" "$result_short"
+output_xml "$result_long" "$result_short"
